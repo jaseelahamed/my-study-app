816fea1fe07e894ae28a6da9a93a9167682ed710
jaseel ahammed <jaseelkp927@gmail.com>
2026-04-01 23:28:29 +0530

    final fix: correct yaml configuration

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
